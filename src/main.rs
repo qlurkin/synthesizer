c817a03b190fb62390772ab4da1ebe05f324d3c0
Quentin Lurkin <qlurkin@gmail.com>
2024-03-12 00:17:29 +0100

    Go for cpal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use rodio::{OutputStream, Source};
-use std::time::Duration;
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    SizedSample,
+};
+use cpal::{FromSample, Sample};
 
 const SAMPLE_RATE: u32 = 44100;
 
@@ -11,69 +14,184 @@ fn _volume_to_db(volume: f32) -> f32 {
     20.0_f32 * volume.log10()
 }
 
-#[inline]
 fn w(freq: f32) -> f32 {
     freq * 2.0 * std::f32::consts::PI
 }
 
-#[derive(Clone, Debug)]
-struct Oscillator {
-    freq: f32,
-    sample_rate: u32,
-    num_sample: usize,
+pub enum Waveform {
+    Sine,
+    Square,
+    Saw,
+    Triangle,
+}
+
+pub struct Oscillator {
+    pub sample_rate: f32,
+    pub waveform: Waveform,
+    pub current_sample_index: f32,
+    pub frequency_hz: f32,
 }
 
 impl Oscillator {
-    #[inline]
-    fn new(freq: f32, sample_rate: u32) -> Self {
-        Self {
-            freq,
-            sample_rate,
-            num_sample: 0,
+    fn advance_sample(&mut self) {
+        self.current_sample_index = (self.current_sample_index + 1.0) % self.sample_rate;
+    }
+
+    fn set_waveform(&mut self, waveform: Waveform) {
+        self.waveform = waveform;
+    }
+
+    fn calculate_sine_output_from_freq(&self, freq: f32) -> f32 {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        (self.current_sample_index * freq * two_pi / self.sample_rate).sin()
+    }
+
+    fn is_multiple_of_freq_above_nyquist(&self, multiple: f32) -> bool {
+        self.frequency_hz * multiple > self.sample_rate / 2.0
+    }
+
+    fn sine_wave(&mut self) -> f32 {
+        self.advance_sample();
+        self.calculate_sine_output_from_freq(self.frequency_hz)
+    }
+
+    fn generative_waveform(&mut self, harmonic_index_increment: i32, gain_exponent: f32) -> f32 {
+        self.advance_sample();
+        let mut output = 0.0;
+        let mut i = 1;
+        while !self.is_multiple_of_freq_above_nyquist(i as f32) {
+            let gain = 1.0 / (i as f32).powf(gain_exponent);
+            output += gain * self.calculate_sine_output_from_freq(self.frequency_hz * i as f32);
+            i += harmonic_index_increment;
+        }
+        output
+    }
+
+    fn square_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 1.0)
+    }
+
+    fn saw_wave(&mut self) -> f32 {
+        self.generative_waveform(1, 1.0)
+    }
+
+    fn triangle_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 2.0)
+    }
+
+    fn tick(&mut self) -> f32 {
+        match self.waveform {
+            Waveform::Sine => self.sine_wave(),
+            Waveform::Square => self.square_wave(),
+            Waveform::Saw => self.saw_wave(),
+            Waveform::Triangle => self.triangle_wave(),
         }
     }
 }
 
-impl Iterator for Oscillator {
-    type Item = f32;
+pub fn stream_setup_for() -> Result<cpal::Stream, anyhow::Error>
+where
+{
+    let (_host, device, config) = host_device_setup()?;
 
-    #[inline]
-    fn next(&mut self) -> Option<f32> {
-        self.num_sample = self.num_sample.wrapping_add(1);
-        Some((w(self.freq) * self.num_sample as f32 / self.sample_rate as f32).sin() * 0.3)
+    match config.sample_format() {
+        cpal::SampleFormat::I8 => make_stream::<i8>(&device, &config.into()),
+        cpal::SampleFormat::I16 => make_stream::<i16>(&device, &config.into()),
+        cpal::SampleFormat::I32 => make_stream::<i32>(&device, &config.into()),
+        cpal::SampleFormat::I64 => make_stream::<i64>(&device, &config.into()),
+        cpal::SampleFormat::U8 => make_stream::<u8>(&device, &config.into()),
+        cpal::SampleFormat::U16 => make_stream::<u16>(&device, &config.into()),
+        cpal::SampleFormat::U32 => make_stream::<u32>(&device, &config.into()),
+        cpal::SampleFormat::U64 => make_stream::<u64>(&device, &config.into()),
+        cpal::SampleFormat::F32 => make_stream::<f32>(&device, &config.into()),
+        cpal::SampleFormat::F64 => make_stream::<f64>(&device, &config.into()),
+        sample_format => Err(anyhow::Error::msg(format!(
+            "Unsupported sample format '{sample_format}'"
+        ))),
     }
 }
 
-impl Source for Oscillator {
-    #[inline]
-    fn channels(&self) -> u16 {
-        1
-    }
+pub fn host_device_setup(
+) -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
+    let host = cpal::default_host();
 
-    #[inline]
-    fn sample_rate(&self) -> u32 {
-        self.sample_rate
-    }
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
+    println!("Output device : {}", device.name()?);
 
-    #[inline]
-    fn current_frame_len(&self) -> Option<usize> {
-        None
-    }
+    let config = device.default_output_config()?;
+    println!("Default output config : {:?}", config);
 
-    #[inline]
-    fn total_duration(&self) -> Option<Duration> {
-        None
+    Ok((host, device, config))
+}
+
+pub fn make_stream<T>(
+    device: &cpal::Device,
+    config: &cpal::StreamConfig,
+) -> Result<cpal::Stream, anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let num_channels = config.channels as usize;
+    let mut oscillator = Oscillator {
+        waveform: Waveform::Sine,
+        sample_rate: config.sample_rate.0 as f32,
+        current_sample_index: 0.0,
+        frequency_hz: 440.0,
+    };
+    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+
+    let time_at_start = std::time::Instant::now();
+    println!("Time at start: {:?}", time_at_start);
+
+    let stream = device.build_output_stream(
+        config,
+        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
+            // for 0-1s play sine, 1-2s play square, 2-3s play saw, 3-4s play triangle_wave
+            let time_since_start = std::time::Instant::now()
+                .duration_since(time_at_start)
+                .as_secs_f32();
+            if time_since_start < 1.0 {
+                oscillator.set_waveform(Waveform::Sine);
+            } else if time_since_start < 2.0 {
+                oscillator.set_waveform(Waveform::Triangle);
+            } else if time_since_start < 3.0 {
+                oscillator.set_waveform(Waveform::Square);
+            } else if time_since_start < 4.0 {
+                oscillator.set_waveform(Waveform::Saw);
+            } else {
+                oscillator.set_waveform(Waveform::Sine);
+            }
+            process_frame(output, &mut oscillator, num_channels)
+        },
+        err_fn,
+        None,
+    )?;
+
+    Ok(stream)
+}
+
+fn process_frame<SampleType>(
+    output: &mut [SampleType],
+    oscillator: &mut Oscillator,
+    num_channels: usize,
+) where
+    SampleType: Sample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(num_channels) {
+        let value: SampleType = SampleType::from_sample(oscillator.tick());
+
+        // copy the same value to all channels
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
     }
 }
 
-fn main() {
-    let freq: f32 = 440.0;
-
-    let oscillator = Oscillator::new(freq, SAMPLE_RATE);
-
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-    let _restult = stream_handle.play_raw(oscillator);
-
-    std::thread::sleep(Duration::from_secs(2));
+fn main() -> anyhow::Result<()> {
+    let stream = stream_setup_for()?;
+    stream.play()?;
+    std::thread::sleep(std::time::Duration::from_millis(4000));
+    Ok(())
 }
