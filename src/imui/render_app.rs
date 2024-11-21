671165983338e434d9b6fa95304f6b6bce43a97d
Quentin Lurkin <qlurkin@gmail.com>
2024-11-21 23:27:42 +0100

    Play

--- src/imui/render_app.rs
+++ src/imui/render_app.rs
@@ -2,11 +2,19 @@ use ratatui::layout::Rect;
 
 use super::{
     block::block, editable_note::editable_note, frame_context::FrameContext,
-    keyboard::process_raw_input, state::State,
+    keyboard::process_raw_input, message::Message, state::State,
 };
 
 pub fn render_app(state: &mut State, area: Rect, ctx: &mut FrameContext) {
     process_raw_input(&mut state.keyboard, ctx);
+
+    ctx.process_messages(|msg, _msgs| {
+        if let Message::Input(super::keyboard::InputMessage::Play) = msg {
+            state.tracker.play_note();
+            return true;
+        }
+        false
+    });
 
     let inner = block("TermTracker", area, ctx);
 
