d545c24026b59ad0362746fc279cf94c3fc64f9b
Quentin Lurkin <qlurkin@gmail.com>
2025-03-10 19:22:44 +0100

    Basic Mixer in Python

--- py_experiment/test_miniaudio.py
+++ py_experiment/test_miniaudio.py
@@ -1,50 +1,73 @@
 import miniaudio
 import numpy as np
 
-# Paramètres audio
 SAMPLE_RATE = 44100  # Hz
 FREQUENCY = 440.0  # Hz (La4)
-AMPLITUDE = 0.5  # Volume (entre 0 et 1)
-BUFFER_SIZE = 512  # Nombre d'échantillons par bloc
 
 
-def sine_wave_generator():
-    frames = yield b""
-
-    """Générateur d'onde sinusoïdale en temps réel pour miniaudio"""
+def sine_oscilator(frequency: float):
+    frames = yield np.array([])
     phase = 0.0
-    phase_increment = (2.0 * np.pi * FREQUENCY) / SAMPLE_RATE
+    phase_increment = (2.0 * np.pi * frequency) / SAMPLE_RATE
 
     while True:
-        # Génération du buffer de samples
-        # samples = (
-        #     AMPLITUDE * np.sin(phase + np.arange(BUFFER_SIZE) * phase_increment)
-        # ).astype(np.float32)
-        t = np.arange(frames) / SAMPLE_RATE
-
-        samples = AMPLITUDE * np.sin(2 * np.pi * FREQUENCY * t + phase).astype(
-            np.float32
-        )
-
-        # phase = (phase + BUFFER_SIZE * phase_increment) % (2.0 * np.pi)
-        phase += frames * phase_increment
-
-        # miniaudio attend un buffer sous forme de bytes
-        frames = yield samples.tobytes()
+        samples = np.sin(phase + np.arange(frames) * phase_increment)
+        phase = (phase + frames * phase_increment) % (2.0 * np.pi)
+        frames = yield samples
 
 
-# Création du périphérique audio
+def ads(attack: float, decay: float, sustain: float):
+    frames = yield np.array([])
+    elapsed_frames = 0
+
+
+def no_sound():
+    frames = yield np.array([])
+    while True:
+        frames = yield np.zeros(frames)
+
+
+NO_SOUND = no_sound()
+NO_SOUND.send(None)
+
+tracks = [NO_SOUND] * 8
+master_volume = 0.5
+
+
+def noise_maker():
+    frames = yield b""
+
+    while True:
+        samples = np.zeros(frames)
+        for track in tracks:
+            samples += track.send(frames)
+        samples *= master_volume
+        frames = yield samples.astype(np.float32).tobytes()
+
+
 device = miniaudio.PlaybackDevice(
     output_format=miniaudio.SampleFormat.FLOAT32, sample_rate=SAMPLE_RATE, nchannels=1
 )
 
-sine = sine_wave_generator()
+noise = noise_maker()
 
-next(sine)
+noise.send(None)  # same as next(sine) to start generator
 
-# Démarrer la lecture en utilisant le générateur
-device.start(sine)
+device.start(noise)
 
-# Maintenir l’exécution
+input("Appuyez sur Entrée pour arrêter...\n")
+
+sine = sine_oscilator(FREQUENCY)
+sine.send(None)
+
+tracks[0] = sine
+
+input("Appuyez sur Entrée pour arrêter...\n")
+
+sine = sine_oscilator(FREQUENCY * 0.5)
+sine.send(None)
+
+tracks[1] = sine
+
 input("Appuyez sur Entrée pour arrêter...\n")
 device.stop()
